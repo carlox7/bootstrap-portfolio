2d6095cdf64b82617bd9b6f28336a8adca375661
Carlo Santos <carlox7@gmail.com>
2018-02-21 00:32:47 -0800

    made video splash page

---
+++ main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Get the video
+var video = document.getElementById("splashVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "Play";
+    }
+}
